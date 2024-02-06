a2240699fafefa23730812288aa781fc1d525c0a
Abhishek <abhishekb@cdac.in>
2024-02-06 15:32:20 +0530

    added templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, world!"
+    user = {'username': 'Abhishek'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'A post from John'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'A post from Susan'
+        },
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
